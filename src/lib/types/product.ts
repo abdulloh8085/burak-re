3c9788a7e7bdd67d10e5a6c642c61b4ca8aa0b28
Abdulloh <abdulloh.im4@gmail.com>
2026-07-18 12:25:25 +0900

    Feat: develop AppRootState and HomePage type integration

--- src/lib/types/product.ts
+++ src/lib/types/product.ts
@@ -1,4 +1,4 @@
-import { ProductCollection, ProductSize, ProductStatus } from "../enums/product.enum";
+import { ProductCollection, ProductSize, ProductStatus } from "../data/enums/product.enum";
 
 export interface Product {
     _id: string;
