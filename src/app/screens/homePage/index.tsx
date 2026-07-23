44bf556494db440e636e6e1d40be912a52685246
Abdulloh <abdulloh.im4@gmail.com>
2026-07-23 19:29:47 +0900

    fix: developed also newDishes logic to retrieve data from backend

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -10,58 +10,70 @@ import "../../../css/home.css"
 
 import { useDispatch } from 'react-redux'
 import { Dispatch } from '@reduxjs/toolkit'
-
-import { setPopularDishes } from './slice'
-
+import { setNewDishes, setPopularDishes } from './slice'
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
 
 /** REDUX SLICE and SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
-    setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+    setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 })
 
 
-
 export default function HomePage() {
-    const { setPopularDishes } = actionDispatch(useDispatch())
+    const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch())
+// Selector: Store => Data
 
-    // Selector: Store => Data
+
+useEffect(() => {
+// BACKEND server data request => Data
+const product = new ProductService();
+
+product.getProducts({
+page: 1,
+limit: 4,
+order: "productViews",
+productCollection: ProductCollection.DISH
+})
+.then(
+(data) => {
+console.log("data pssed here", data)
+setPopularDishes(data)
+}
+)
+            .catch((err) => { console.log(err) });
 
 
-    useEffect(() => {
-        // BACKEND server data request => Data
-        const product = new ProductService();
+
 
         product.getProducts({
             page: 1,
             limit: 4,
-            order: "productViews",
+            order: "createdAt",
             productCollection: ProductCollection.DISH
         })
             .then(
                 (data) => {
-                    console.log("data pssed here", data)
-                    setPopularDishes(data)
+                    console.log("data pssed here newDishes", data)
+                    setNewDishes(data)
                 }
             )
-            .catch((err) => { console.log(err) })
-
-        // Slice: Data => Store
+.catch((err) => { console.log(err) })
 
 
-    }, [])
+}, [])
 
-    // console.log(process.env.REACT_APP_API_URL)
-    
+// console.log(process.env.REACT_APP_API_URL)
 
-    return <div className={"homepage"} >
-        <Statistics />
-        <PopularDishes />
-        <NewDishes />
-        <Advertisement />
-        <ActiveUsers />
-        <Events />
-    </div>;
+
+return <div className={"homepage"} >
+<Statistics />
+<PopularDishes />
+<NewDishes />
+<Advertisement />
+<ActiveUsers />
+<Events />
+</div>;
 }
