b6cebc52afcc84204b587a886dc414fd2f881639
Abdulloh <abdulloh.im4@gmail.com>
2026-07-23 19:33:25 +0900

    feat: retrieve topUsers and modify ActiveUsers components

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -10,39 +10,42 @@ import "../../../css/home.css"
 
 import { useDispatch } from 'react-redux'
 import { Dispatch } from '@reduxjs/toolkit'
-import { setNewDishes, setPopularDishes } from './slice'
+import { setNewDishes, setPopularDishes, setTopUsers } from './slice'
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
 
 /** REDUX SLICE and SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
-setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+    setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
     setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
+    setTopUsers: (data: Member[]) => dispatch(setTopUsers(data))
 })
 
 
 export default function HomePage() {
-    const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch())
-// Selector: Store => Data
+    const { setPopularDishes, setNewDishes, setTopUsers } = actionDispatch(useDispatch())
+    // Selector: Store => Data
 
 
-useEffect(() => {
-// BACKEND server data request => Data
-const product = new ProductService();
+    useEffect(() => {
+        // BACKEND server data request => Data
+        const product = new ProductService();
 
-product.getProducts({
-page: 1,
-limit: 4,
-order: "productViews",
-productCollection: ProductCollection.DISH
-})
-.then(
-(data) => {
-console.log("data pssed here", data)
-setPopularDishes(data)
-}
-)
+        product.getProducts({
+            page: 1,
+            limit: 4,
+            order: "productViews",
+            productCollection: ProductCollection.DISH
+        })
+            .then(
+                (data) => {
+                    console.log("data pssed here", data)
+                    setPopularDishes(data)
+                }
+            )
             .catch((err) => { console.log(err) });
 
 
@@ -60,20 +63,31 @@ setPopularDishes(data)
                     setNewDishes(data)
                 }
             )
-.catch((err) => { console.log(err) })
+            .catch((err) => { console.log(err) })
 
 
-}, [])
+        //MEMBER
 
-// console.log(process.env.REACT_APP_API_URL)
+        const member = new MemberService();
+
+        member.getTopUsers()
+            .then((data) => {
+                setTopUsers(data)
+            })
+            .catch((err) => console.log(err))
 
 
-return <div className={"homepage"} >
-<Statistics />
-<PopularDishes />
-<NewDishes />
-<Advertisement />
-<ActiveUsers />
-<Events />
-</div>;
+    }, [])
+
+    // console.log(process.env.REACT_APP_API_URL)
+
+
+    return <div className={"homepage"} >
+        <Statistics />
+        <PopularDishes />
+        <NewDishes />
+        <Advertisement />
+        <ActiveUsers />
+        <Events />
+    </div>;
 }
