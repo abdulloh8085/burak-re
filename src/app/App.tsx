2f48d9fffdaa5cd97cb804e3cec0be508976cc88
Abdulloh <abdulloh.im4@gmail.com>
2026-07-28 19:04:58 +0900

    Feat: develop Basket logic

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
-import { Route, Switch, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+// import { RippleBadge } from "./MaterialTheme/styled";
+import { Switch, Link, Route, useLocation } from "react-router-dom";
 import HomePage from "./screens/homePage";
 import ProductsPage from "./screens/productsPage";
 import OrdersPage from "./screens/ordersPage";
@@ -7,21 +9,54 @@ import UserPage from "./screens/userPage";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
 import Footer from "./components/footer";
-import HelpPage from "./screens/helpPage";
+// @ts-ignore: Allow side-effect CSS import without type declarations
 import "../css/app.css";
-import "../css/navbar.css";
-import "../css/footer.css";
+// @ts-ignore: Allow side-effect CSS import without type declarations
+import '../css/navbar.css'
+// @ts-ignore: Allow side-effect CSS import without type declarations
+import '../css/footer.css'
+import HelpPage from "./screens/helpPage";
 import Test from "./screens/Test";
+import { CartItem } from "../lib/types/search";
+
 
 function App() {
   const location = useLocation();
 
+  const cartJson: string | null = localStorage.getItem("cartData");
+  const currentCart = cartJson ? JSON.parse(cartJson) : []
+  const [cartItems, setCartItems] = useState<CartItem[]>(currentCart)
+
+  //** HANDLERS */
+
+  const onAdd = (input: CartItem) => {
+    const exist: any = cartItems.find((item: CartItem) => {
+      return item._id === input._id                            // cartItems bizda mavjud savat b-b items mavjud ichidagiu productrlar input kirgizmoqachi bolgan product
+    });
+
+    if (exist) {
+      const cartUpdate = cartItems.map((item: CartItem) => {
+        return item._id === input._id ? { ...exist, quantity: exist.quantity + 1 } : item
+      });
+
+      setCartItems(cartUpdate)
+      localStorage.setItem("cartData", JSON.stringify(cartUpdate));
+
+    } else {
+      const cartUpdate = [...cartItems, { ...input }];   // agar osha product savatchada bolmasa qoshmoqchi bolgan osha productni savatga qoshib yangi array beradi 
+      setCartItems(cartUpdate);
+
+      localStorage.setItem("cartData", JSON.stringify(cartUpdate));
+    }
+  }
+
+
   return (
     <>
-      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+      {location.pathname === "/" ? <HomeNavbar cartItems={cartItems} /> : <OtherNavbar cartItems={cartItems} />}
       <Switch>
         <Route path="/products">
-          <ProductsPage />
+          <ProductsPage onAdd={onAdd} />
         </Route>
         <Route path="/orders">
           <OrdersPage />
@@ -33,18 +68,13 @@ function App() {
           <HelpPage />
         </Route>
         <Route path="/">
-        
-           <HomePage />
+          <HomePage />
         </Route>
       </Switch>
       <Footer />
-    </>
-  );
+    </>)
 }
 
+
+
 export default App;
-
-
-
-
-
